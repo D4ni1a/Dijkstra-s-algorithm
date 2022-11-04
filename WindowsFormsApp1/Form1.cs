767e6aab3642d3612c9c1523265416d6f2b8714c
D4ni1a <71354878+D4ni1a@users.noreply.github.com>
2022-11-04 17:28:04 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public static Dictionary<int, float[]> Dict;
+        Graphics gr;
+        Random rnd;
+        int[,] myArray;
+        int n;
+        int count=0;
+        Dictionary<string, int> D;
+        public Form1()
+        {
+            InitializeComponent();
+            textBox2.Hide();
+            textBox3.Hide();
+            textBox4.Hide();
+            button2.Hide();
+            button3.Hide();
+
+            label2.Hide();
+            label3.Hide();
+            label4.Hide();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+        
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                n = System.Convert.ToInt32(textBox1.Text);
+                if ((n==0) || (n==1) || (n==2))
+                {
+                    MessageBox.Show("Ошибка!");
+                    return;
+                }
+                myArray = new int[n, n];
+                rnd = new Random();
+                gr = CreateGraphics();
+                Pen pen = new Pen(Color.FromArgb(255, 0, 0, 0));
+                Dict = new Dictionary<int, float[]>(n);
+                Font font = new Font("Times New Roman", 10);
+                SolidBrush drawBrush = new SolidBrush(Color.Black);
+                for (int i = 0; i < n; i++)
+                {
+                    float[] point = new float[2];
+                    float x_con = rnd.Next(20, 300);
+                    float y_con = rnd.Next(20, 300);
+                    point[0] = x_con;
+                    point[1] = y_con;
+                    gr.DrawEllipse(pen, x_con - 2, y_con - 2, 4, 4);
+                    gr.DrawString(System.Convert.ToString(i + 1), font, drawBrush, x_con, y_con - 15);
+                    Dict[i] = point;
+                }
+
+                textBox1.Hide();
+                textBox2.Show();
+                textBox3.Show();
+                textBox4.Show();
+                button2.Show();
+                button3.Show();
+                button1.Hide();
+                label1.Hide();
+                label2.Show();
+                label3.Show();
+                label4.Show();
+
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка!");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Pen pen = new Pen(Color.Red);
+                int s1 = System.Convert.ToInt32(textBox2.Text) - 1;
+                int s2 = System.Convert.ToInt32(textBox3.Text) - 1;
+
+                float deltaX = (Dict[s1][0] + Dict[s2][0]) / 2;
+                float deltaY = (Dict[s1][1] + Dict[s2][1]) / 2;
+                Font font = new Font("Times New Roman", 9);
+                SolidBrush drawBrush = new SolidBrush(Color.Blue);
+                gr.DrawString(textBox4.Text, font, drawBrush, deltaX+5, deltaY - 5);
+
+                gr.DrawLine(pen, Dict[s1][0], Dict[s1][1], Dict[s2][0], Dict[s2][1]);
+                myArray[s1, s2] = System.Convert.ToInt32(textBox4.Text);
+                myArray[s2, s1] = System.Convert.ToInt32(textBox4.Text);
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                count += 1;
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка!");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                D = new Dictionary<string, int>(count);
+                string mas;
+                List<int> tops = new List<int>();
+
+                Dictionary<string, int> D1 = new Dictionary<string, int>();
+
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        mas = "";
+                        if (myArray[i, j] != 0)
+                        {
+                            if (i < j)
+                            {
+                                mas += i;
+                                mas += " ";
+                                mas += j;
+                            }
+                            else
+                            {
+                                mas += j;
+                                mas += " ";
+                                mas += i;
+                            }
+                            D.Add(mas, myArray[i, j]);
+                        }
+                    }
+                }
+                D = D.OrderBy(pair => pair.Value).ToDictionary(pair => pair.Key, pair => pair.Value);
+
+                int cycle;
+                List<string> vershinas=new List<string>();
+
+                foreach (var item in D)
+                {
+                    
+                    cycle = 0;
+                    
+                    D1.Add(item.Key, item.Value);
+                    vershinas.Add(item.Key);
+                    cycle = Program.Cycle(vershinas, n);
+                    if (cycle==1)
+                    {
+                        D1.Remove(item.Key);
+                        vershinas.Remove(item.Key);
+                    }
+                }
+
+                gr.Clear(Color.White);
+                
+
+                Pen pen = new Pen(Color.Black);
+                Font font = new Font("Times New Roman", 10);
+                SolidBrush drawBrush = new SolidBrush(Color.Black);
+
+                for (int i = 0; i < n; i++)
+                {
+                    float x_con = Dict[i][0];
+                    float y_con = Dict[i][1];
+
+                    gr.DrawEllipse(pen, x_con - 2, y_con - 2, 4, 4);
+                    gr.DrawString(System.Convert.ToString(i + 1), font, drawBrush, x_con, y_con - 15);
+
+                }
+                int sum = 0;
+                int k1;
+                int k2;
+                foreach (var item in D1)
+                {
+                    k1 = Int32.Parse(item.Key.Split()[0]);
+                    k2 = Int32.Parse(item.Key.Split()[1]);
+                    Pen penw = new Pen(Color.Red);
+
+                    float deltaX = (Dict[k1][0] + Dict[k2][0]) / 2;
+                    float deltaY = (Dict[k1][1] + Dict[k2][1]) / 2;
+                    Font font1 = new Font("Times New Roman", 9);
+                    SolidBrush drawBrush1 = new SolidBrush(Color.Blue);
+                    gr.DrawString(item.Value.ToString(), font1, drawBrush1, deltaX + 5, deltaY - 5);
+
+                    gr.DrawLine(penw, Dict[k1][0], Dict[k1][1], Dict[k2][0], Dict[k2][1]);
+                    sum+=item.Value;
+                }
+                textBox2.Hide();
+                textBox3.Hide();
+                textBox4.Hide();
+                button2.Hide();
+                button3.Hide();
+                label2.Hide();
+                label3.Hide();
+                label4.Hide();
+                MessageBox.Show("Длина минимального остовного дерева: "+System.Convert.ToString(sum));
+            }
+            catch
+            {
+                MessageBox.Show("Ошибка!");
+            }
+        }
+    }
+}
--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -1,1 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
 
+namespace WindowsFormsApp1
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+        public static int Cycle(List<string> Dict, int num)
+        {
+            List<string> D = Dict;
+            int count = 0;
+            string versh = "";
+
+            for (int i=0; i<num; i++)
+            {
+                versh += i.ToString();
+            }
+
+            foreach (char t in versh)
+            {
+                int v_last = 0;
+                int v_prev = 0;
+                int v_now = Int32.Parse(System.Convert.ToString(t));
+                string seq;
+                int z = 0;
+                int n = 0;
+                seq = System.Convert.ToString(t);
+
+                while (z < 5)
+                {
+                    int kk = 0;
+                    while (true)
+                    {
+                        n += 1;
+                        kk = seq.Length;
+                        foreach (var item in D)
+                        {
+
+                            if ((Int32.Parse(item.Split()[0]) == v_now) && (Int32.Parse(item.Split()[1]) != v_last) && (Int32.Parse(item.Split()[1]) != v_prev))
+                            {
+                                seq += item.Split()[1];
+                                v_now = Int32.Parse(item.Split()[1]);
+                                v_prev = Int32.Parse(item.Split()[0]);
+                                v_last = Int32.Parse(item.Split()[1]);
+                                break;
+                            }
+                            if ((Int32.Parse(item.Split()[1]) == v_now) && (Int32.Parse(item.Split()[0]) != v_last) && (Int32.Parse(item.Split()[0]) != v_prev))
+                            {
+                                seq += item.Split()[0];
+                                v_now = Int32.Parse(item.Split()[0]);
+                                v_prev = Int32.Parse(item.Split()[1]);
+                                v_last = Int32.Parse(item.Split()[0]);
+                                break;
+                            }
+
+                        }
+                        if ((kk == seq.Length) || (n == 20)) break;
+                    }
+                    if ((seq[0] == seq[seq.Length - 1]) && (seq.Length != 1))
+                    {
+                        return 1;
+                    }
+
+                    if (seq.Length >= 3)
+                    {
+                        v_last = Int32.Parse(System.Convert.ToString(seq[seq.Length - 1]));
+                        v_now = Int32.Parse(System.Convert.ToString(seq[seq.Length - 2]));
+                        v_prev = Int32.Parse(System.Convert.ToString(seq[seq.Length - 3]));
+                    }
+                    else
+                    {
+                        v_last = Int32.Parse(System.Convert.ToString(seq[seq.Length - 1]));
+                        v_prev = 0;
+                        v_now = Int32.Parse(System.Convert.ToString(seq[0]));
+                    }
+                    string seq1 = System.Convert.ToString(t);
+                    for (int s = 1; s < seq.Length - 1; s++)
+                    {
+                        seq1 += seq[s];
+                    }
+                    seq = seq1;
+                    z += 1;
+                }
+            }
+            return 0;
+        }
+    }
+}
